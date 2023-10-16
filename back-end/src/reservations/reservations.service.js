2cbdd247e8446a1a1001f6ab58a16b4db4c08ee8
aries grey <ariesgrey88@gmail.com>
2023-10-16 14:24:42 -0500

    US-07 backend complete, all tests passing

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -33,9 +33,20 @@ function update(updatedReservation) {
 		.then((returnedRecords) => returnedRecords[0]);
 }
 
+// Search for reservations by phone number
+function search(mobile_number) {
+	return knex("reservations")
+		.whereRaw(
+			"translate(mobile_number, '() -', '') like ?",
+			`%${mobile_number.replace(/\D/g, "")}%`
+		)
+		.orderBy("reservation_date");
+}
+
 module.exports = {
 	list,
 	create,
 	read,
 	update,
+	search,
 };
